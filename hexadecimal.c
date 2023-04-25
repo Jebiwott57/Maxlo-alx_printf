ce92edc190d034c554465e40d459c3e82671bf46
Ghost349 <maxwellmuriungi11@gmail.com>
2023-04-25 12:49:46 -0700

    hexadecimal

---
+++ hexadecimal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * add_hexadecimal - Add ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @s: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int add_hexadecimal(char ascii_code, char buffer[], int s)
+{
+        char map_to[] = "0123456789ABCDEF";
+
+        if (ascii_code < 0)
+                ascii_code *= -1;
+
+        buffer[s++] = '\\';
+        buffer[s++] = 'x';
+
+        buffer[s++] = map_to[ascii_code / 16];
+        buffer[s] = map_to[ascii_code % 16];
+
+        return (3);
+}
