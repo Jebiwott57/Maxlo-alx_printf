bf67d6bd2e802dfb9de93fb44f902321d521e8bb
Jebiwott57 <terikij@mail.com>
2023-04-25 19:45:41 +0000

    A function that adds hexadecimal to the buffer

---
+++ hexadecimal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * add_hexadecimal - A function that adds ascci in hexadecimal
+ * to buffer for output.
+ * @buffer: Array of chars.
+ * @s: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int add_hexadecimal(char ascii_code, char buffer[], int s)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[s++] = '\\';
+	buffer[s++] = 'x';
+
+	buffer[s++] = map_to[ascii_code / 16];
+	buffer[s] = map_to[ascii_code % 16];
+
+	return (3);
+}
