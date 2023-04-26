ecb521cfa9de8216a9f047f973f70bcd16b2220f
Ghost349 <maxwellmuriungi11@gmail.com>
2023-04-26 08:55:16 -0700

    func_char

---
+++ func_char.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*----------------------- PRINT CHAR -------------------------*/
+
+/**
+ * print_char - a function that prints a char
+ * @types: List a of arguments
+ * @buffer: The  array to print a character
+ * @flags:  Determine active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
+ */
+int print_char(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
