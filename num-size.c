e3d590714858a5a08a572dc12b839191e0926e8a
Jebiwott57 <terikij@mail.com>
2023-04-26 21:56:12 +0000

    Function that prints number of output size

---
+++ num-size.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * convert_size_number - Function that casts number to specified
+ * size of output.
+ * @num: Number to be cast to output.
+ * @size: the Number to specify cast.
+ *
+ * Return: num.
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+	return (num);
+
+	else if (size == S_SHORT)
+		return (num);
+
+	return (num);
+}
