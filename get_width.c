130da53986018eb1187c5555c2b60d7da2a7385a
Ghost349 <maxwellmuriungi11@gmail.com>
2023-04-26 15:53:33 -0700

    printf width update

--- get_width.c
+++ get_width.c
@@ -1,13 +1,8 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * get_width - A function that prints the width.
- * @format: A pointer pointing to the input argument to print.
-=======
  * get_width - a function that prints the width
  * @format: a pointer pointing to the argument to print
->>>>>>> max-practice
  * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
@@ -18,12 +13,9 @@ int get_width(const char *format, int *i, va_list list)
 	int math;
 	int width;
 
-<<<<<<< HEAD
-	for (math = *i + 1; format[math] != '\0'; math++)
-=======
 	math = *i + 1;
 	for (; format[math] != '\0'; math++)
->>>>>>> max-practice
+
 	{
 		width = 0;
 		if (is_digit(format[math]))
