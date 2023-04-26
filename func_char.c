5cb21cf5ec347c6638f862be3b203b54afec5346
Jebiwott57 <terikij@mail.com>
2023-04-26 18:20:09 +0000

    Function that prints character

---
+++ func_char.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_char - A function that printing chararacter.
+ * @types: Arguments to print.
+ * @buffer: The  array to print a character.
+ * @flags:  Determine active flags.
+ * @width: Width of output.
+ * @precision: Precision specification for output.
+ * @size: Size specifier for output.
+ * Return: handle_write_char
+ */
+int print_char(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
